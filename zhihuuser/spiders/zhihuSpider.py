1ab9c6edde86d8659c4aa50ccb49b9a25dd81d95
缪超青 <419666193@qq.com>
2018-04-29 21:24:45 +0800

    'first_commit'

---
+++ zhihuuser/spiders/zhihuSpider.py
@@ -0,0 +1,16 @@
+# -*- coding: utf-8 -*-
+import scrapy
+from scrapy import Spider, Request
+
+
+class ZhihuspiderSpider(scrapy.Spider):
+    name = 'zhihuSpider'
+    allowed_domains = ['www.zhihu.com']
+    start_urls = ['http://www.zhihu.com/']
+
+    def start_requests(self):
+        url = 'https://www.zhihu.com/api/v4/members/excited-vczh/followees?include=data%5B*%5D.answer_count%2Carticles_count%2Cgender%2Cfollower_count%2Cis_followed%2Cis_following%2Cbadge%5B%3F(type%3Dbest_answerer)%5D.topics&offset=40&limit=20'
+        yield Request(url, callback=self.parse)
+
+    def parse(self, response):
+        print(response.text)
