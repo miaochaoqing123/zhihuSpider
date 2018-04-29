61f47aca93c4f165c71c0047f14b2f4feb32e852
缪超青 <419666193@qq.com>
2018-04-29 21:56:29 +0800

    '改造好,起始的url'

--- zhihuuser/spiders/zhihuSpider.py
+++ zhihuuser/spiders/zhihuSpider.py
@@ -7,10 +7,36 @@ class ZhihuspiderSpider(scrapy.Spider):
     name = 'zhihuSpider'
     allowed_domains = ['www.zhihu.com']
     start_urls = ['http://www.zhihu.com/']
+    start_user = 'excited-vczh'
+
+    # 定义关注人的url常量
+    user_url = 'https://www.zhihu.com/api/v4/members/{user}?include={include}'
+    user_query = 'allow_message%2Cis_followed%2Cis_following%2Cis_org%2Cis_blocking%2Cemployments%2Canswer_count%2Cfollower_count%2Carticles_count%2Cgender%2Cbadge%5B%3F(type%3Dbest_answerer)%5D.topics'
+
+    # 定义所有关注人列表url常量
+    follows_url = 'https://www.zhihu.com/api/v4/members/{user}/followees?include={include}&offset={offset}&limit={limit}'
+    follows_query = 'data[*].answer_count,articles_count,gender,follower_count,is_followed,is_following,badge[?(type=best_answerer)].topics'
 
     def start_requests(self):
-        url = 'https://www.zhihu.com/api/v4/members/excited-vczh/followees?include=data%5B*%5D.answer_count%2Carticles_count%2Cgender%2Cfollower_count%2Cis_followed%2Cis_following%2Cbadge%5B%3F(type%3Dbest_answerer)%5D.topics&offset=40&limit=20'
-        yield Request(url, callback=self.parse)
+        yield Request(self.user_url.format(user=self.start_user,include=self.user_query),self.parse_user)
+        # url = 'https://www.zhihu.com/api/v4/members/excited-vczh/followees?include=data%5B*%5D.answer_count%2Carticles_count%2Cgender%2Cfollower_count%2Cis_followed%2Cis_following%2Cbadge%5B%3F(type%3Dbest_answerer)%5D.topics&offset=40&limit=20'
+        yield Request(self.follows_url.format(user=self.start_user, include=self.follows_query, offset=0, limit=20), callback=self.parse_follows)
 
-    def parse(self, response):
+    def parse_user(self, response):
         print(response.text)
+
+    def parse_follows(self, response):
+        print(response.text)
+
+
+
+
+
+
+
+
+
+
+
+
+
